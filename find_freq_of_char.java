1c7ea1892d00661f722839a341e91a7d3dc6ae33
Rajesh1216-dundangi <rajeshdundangi75@gmail.com>
2026-02-02 15:56:13 +0530

    Create find_freq_of_char.java

---
+++ find_freq_of_char.java
@@ -0,0 +1,18 @@
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == ch) {
+                    count++;
+                }
+            }
+
+            System.out.println(ch + " : " + count);
+        }
+    }
+}
